64618cd34afab7add09cbed4c1c966f9b459ac7c
Edilson Junior <edilson.junior@camara.leg.br>
2023-11-28 15:50:59 -0300

    chore: Altera intervalo de minutos

--- notifica_zap_mauricio.py
+++ notifica_zap_mauricio.py
@@ -11,16 +11,16 @@ def notificacao(mensagem):
     litros = (copo * 200) / 1000
     # subprocess.Popen(['notify-send', f"{mensagem} Atingido {litros} Litros"])
     texto = f"Tome água (200ml). Atingido: {litros} litros"
-    requests.get(f"http://api.callmebot.com/whatsapp.php?phone=556198845429&text={texto}&apikey=6904746")
+    requests.get(f"http://api.callmebot.com/whatsapp.php?phone=557799376857&text={texto}&apikey=4359407")
 
 
-schedule.every(3600).seconds.do(notificacao, mensagem="")
+schedule.every(1800).seconds.do(notificacao, mensagem="")
 
 while True:
     schedule.run_pending()
     time.sleep(1)
     # Quando a quantidade de água ingerida chegar a 4 litros o programa é encerrado
     if litros >= 3:
-        texto = "Você já atingiu a meta. Parabéns linda!"
-        requests.get(f"http://api.callmebot.com/whatsapp.php?phone=556198845429&text={texto}&apikey=6904746")
+        texto = "Você já atingiu a meta. Parabéns, Tchawly!"
+        requests.get(f"http://api.callmebot.com/whatsapp.php?phone=557799376857&text={texto}&apikey=4359407")
         exit(0)
